15b9348c71fd8a266e01db40a57982ed5801e6cf
Davide <davidel96@hotmail.it>
2020-02-10 15:37:14 +0100

    Perception draft

--- src/perception/script/perception_node.py
+++ src/perception/script/perception_node.py
@@ -8,7 +8,7 @@ import numpy as np
 from scipy.ndimage import filters
 
 # OpenCV
-import cv2
+from cv2 import cv2
 
 # Image processing
 import matplotlib.pyplot as plt
@@ -20,12 +20,14 @@ import rospy
 
 # Ros Messages
 from sensor_msgs.msg import CompressedImage
+from std_msgs.msg import Float32
 
 
 # _____________________________________________________________________
 
 # Global variables
 VERBOSE=False
+DISPLAY=True
 
 # _____________________________________________________________________
 # "Callback class"
@@ -35,8 +37,9 @@ class image_feature:
     def __init__(self):
         '''Initialize ros publisher, ros subscriber'''
         # topic where we publish
-        self.image_pub = rospy.Publisher("/output/image_raw/compressed",
-            CompressedImage, queue_size=1)
+        #self.image_pub = rospy.Publisher("/output/image_raw/compressed",
+        #    CompressedImage, queue_size=1)
+        self.ballX_pub = rospy.Publisher("ballX", Float32, queue_size=1)
 
         # subscribed Topic
         self.subscriber = rospy.Subscriber("/raspicam_node/image/compressed",
@@ -57,14 +60,42 @@ class image_feature:
         Here images get converted and features detected'''
         if VERBOSE :
             print('received image of type: "%s"' % ros_data.format)
-        # Direct conversion to CV2
         np_arr = np.fromstring(ros_data.data, np.uint8)
+        # Direct conversion to CV2
         frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR) # OpenCV >= 3.0:
+        # Get frame dimensions
         frame_height = frame.shape[0]
         frame_width = frame.shape[1]
         frame_channels = frame.shape[2]
-        cv2.imshow('frame', frame)
-        cv2.waitKey(2)
+        # Image processing
+        res = self.red_filtering(frame)
+        # Detect centrode
+        cX, cY = self.detect_centrode(res)
+        # Write the point (cX,xY) on "res" image
+        cv2.circle(res, (int(cX),int(cY)), 5, (255, 0, 0), -1)
+        # Normalizing w.r.t the center
+        cX = int(cX-frame_width/2) 
+        cY = int(cY-frame_height/2)
+        self.ballX_pub.publish(cX)
+
+        ################## DEBUG STUFF
+
+        # Print the center of mass coordinates w.r.t the center of image and diplay it
+        if VERBOSE:
+            print (cX, cY)
+            cmd = self.extraction(cX)
+            print(cmd)
+        # Display the result
+        if DISPLAY:
+            cv2.imshow('frame', frame)
+            cv2.imshow("res_center",res)
+            cv2.waitKey(2)
+
+
+    def red_filtering(self,frame):        
+        # Adapted from 
+        # https://stackoverflow.com/questions/54425093/
+        # /how-can-i-find-the-center-of-the-pattern-and-the-distribution-of-a-color-around)
 
         # Convert BGR to HSV
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -74,18 +105,21 @@ class image_feature:
         res = cv2.bitwise_and(frame, frame, mask=mask)
         # Smooth and blur "res" image
         res = cv2.medianBlur(res,15)
-        #res = cv2.GaussianBlur(res,(15,15),0)
-        #res = cv2.bilateralFilter(res,15,75,75)
-
-        # Then do the elaboration
-        # (https://stackoverflow.com/questions/54425093/how-can-i-find-the-center-of-the-pattern-and-the-distribution-of-a-color-around)
-        # 1 - Erode and convert it for noise reduction
+            # Or also:
+            # res = cv2.GaussianBlur(res,(15,15),0)
+            # res = cv2.bilateralFilter(res,15,75,75)
+        # Erode and convert it for noise reduction
         kernel = np.ones((2,2),np.uint8)
-        opening = cv2.morphologyEx(res, cv2.MORPH_OPEN, kernel)
-        opening = cv2.cvtColor(opening, cv2.COLOR_BGR2GRAY)
-        opening = cv2.convertScaleAbs(opening)
-        # 2 - Compute the center of mass
-        contours, hierarchy = cv2.findContours(opening, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        res = cv2.morphologyEx(res, cv2.MORPH_OPEN, kernel)
+        res = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+        res = cv2.convertScaleAbs(res)
+        return res
+
+    def detect_centrode(self,res):        
+        # Adapted from 
+        # https://stackoverflow.com/questions/54425093/
+        # /how-can-i-find-the-center-of-the-pattern-and-the-distribution-of-a-color-around)
+        contours, hierarchy = cv2.findContours(res, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         areas = []
         centersX = []
         centersY = []
@@ -106,13 +140,27 @@ class image_feature:
         for i in range(len(areas)):
             acc_X += centersX[i] * (areas[i]/full_areas) 
             acc_Y += centersY[i] * (areas[i]/full_areas)
+        return acc_X,acc_Y
 
-        # Print the center of mass coordinates w.r.t the center of image and diplay it
-        print (int(acc_X-frame_width/2), int(acc_Y-frame_height/2))
-        cv2.circle(res, (int(acc_X), int(acc_Y)), 5, (255, 0, 0), -1)
-        cv2.imshow("res_center",res)
+    
+    def extraction(self, cX):
+        #  cX  >    0 -> ball on the right
+        #  cX  >  100 -> ball on the FAR right
+        #  cX  <    0 -> ball on the left
+        #  cX  < -100 -> ball on the FAR right
+        # -100<cX<100 -> ball ok
 
-        #self.subscriber.unregister()
+        if abs(cX) < 190:
+            if cX >  80:
+                return 'R'
+            elif cX < -100: 
+                return 'L'
+            else:
+                return True    
+        else:
+            return False 
+
+
 
 
 # _____________________________________________________________________
@@ -120,10 +168,13 @@ class image_feature:
 
 def main(args):
     '''Initializes and cleanup ros node'''
+    print("Starting ROS Image feature detector module")
     ic = image_feature()
     rospy.init_node('image_feature', anonymous=True)
+    r = rospy.Rate(1) # 1hz
     try:
         rospy.spin()
+        r.sleep()
     except KeyboardInterrupt:
         print("Shutting down ROS Image feature detector module")
     cv2.destroyAllWindows()
---
+++ start_legobot.sh
@@ -0,0 +1,17 @@
+# source ./start_legobot.sh
+echo "hostname IP is:"
+IP=$(eval "hostname -I")
+echo $IP
+
+echo "setting ROS_IP to:"
+export ROS_IP=$IP
+echo $ROS_IP
+
+echo "setting ROS_HOSTNAME to:"
+export ROS_HOSTNAME=$IP
+echo $ROS_HOSTNAME
+
+echo "setting ROS_MASTER_URI to:"
+CORE_URI="http://legobot.local:11311"
+export ROS_MASTER_URI=$CORE_URI
+echo $ROS_MASTER_URI
