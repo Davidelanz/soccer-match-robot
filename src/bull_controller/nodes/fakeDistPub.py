8c21af5176f6735b637fdfe4297400a9ef3ecca6
AlbertoGhiotto <alberto.ghiotto@hotmail.it>
2020-02-21 12:09:10 +0100

    Dummy publisher now publish via key

--- src/bull_controller/nodes/fakeDistPub.py
+++ src/bull_controller/nodes/fakeDistPub.py
@@ -3,25 +3,55 @@
 import rospy
 from std_msgs.msg import Float32
 
+import sys, select, termios, tty
+
+msg = """
+Reading from the keyboard 
+
+g - sends a good distance
+
+b - sends a bad distance
+
+CTRL-C to quit
+"""
+
+def getKey():
+    tty.setraw(sys.stdin.fileno())
+    select.select([sys.stdin], [], [], 0)
+    key = sys.stdin.read(1)
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
 
 def talker():
     dist_pub = rospy.Publisher("dist", Float32, queue_size=1)
     rospy.init_node('fake_dist')
 
-    counter = 1
-    dist = 100
-    
-    r = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-       fakeDist = dist - counter
-       dist_pub.publish(fakeDist)
-       counter = counter + 1
-       if counter == 101:
-           counter = 0
+    # counter = 1
+    # dist = 100
+        
 
-       r.sleep()
+    
+    print(msg) 
+    while(1):
+        key = getKey()   
+        if key == 'g':
+            fakeDist = 15
+        elif key == 'b':
+            fakeDist = 75
+
+        dist_pub.publish(fakeDist)
+    # r = rospy.Rate(5) # 5hz
+    # while not rospy.is_shutdown():
+    # fakeDist = dist - counter
+    # dist_pub.publish(fakeDist)
+    # counter = counter + 1
+    # if counter == 101:
+    #     counter = 0
+
+      # r.sleep()
 
 if __name__ == '__main__':
+    settings = termios.tcgetattr(sys.stdin)
     try:
         talker()
     except rospy.ROSInterruptException:
